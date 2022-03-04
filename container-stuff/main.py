d59dfe3dd66a82b9e0c7afd476a94abf58f69035
Ognjen Stanisavljevic <ognjen.it@gmail.com>
2022-03-04 10:05:06 +0100

    added init

--- container-stuff/main.py
+++ container-stuff/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Banca Intesa"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
